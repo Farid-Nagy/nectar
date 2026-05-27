89b0af420bad98a340dfcdbf08c68823d01be6a4
Farid Nagy <fareednagy308@gmail.com>
2026-05-27 21:53:12 +0300

    first commint

---
+++ lib/Core/app_coloer.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF53B175);
+  static const Color secondaryColor = Color(0xFFF3603F);
+  static const Color textColor1 = Color(0xFFFFFFFF);
+  static const Color backgroundColor = Color(0xFFFFFFFF);
+  static const Color textColor2 = Color(0xFF000000);
+}
---
+++ lib/auth/presentation/login_screen.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:nectar/Core/app_coloer.dart';
+import 'signup_screen.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 80),
+            Center(
+              child: SvgPicture.asset(
+                'assets/images/Group.svg',
+                color: AppColors.secondaryColor,
+              ),
+            ),
+            SizedBox(height: 100),
+            Text(
+              'Log in',
+              style: GoogleFonts.gabarito(
+                fontSize: 30,
+                color: AppColors.textColor2,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Enter your emails and password',
+              style: GoogleFonts.poppins(
+                fontSize: 16,
+                color: AppColors.textColor2.withOpacity(0.7),
+              ),
+            ),
+            SizedBox(height: 30),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'email@eraaSoft.com',
+                prefixIcon: Icon(Icons.email),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                labelText: '°°°°°°°°',
+                prefixIcon: Icon(Icons.lock),
+                border: OutlineInputBorder(),
+              ),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            Container(
+              child: Text(
+                'Forgot your password?',
+                style: GoogleFonts.poppins(
+                  fontSize: 14,
+                  color: AppColors.textColor2,
+                ),
+              ),
+              alignment: Alignment.centerRight,
+            ),
+            SizedBox(height: 30),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SignupScreen()),
+                  );
+                },
+                child: Text(
+                  'Log in',
+                  style: GoogleFonts.poppins(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: AppColors.primaryColor,
+                  foregroundColor: AppColors.textColor1,
+                  fixedSize: Size(364, 67),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Center(
+              child: Text(
+                'Don’t have an account? Singup',
+                style: GoogleFonts.poppins(
+                  fontSize: 14,
+                  color: AppColors.textColor2,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
